4c36ef53c98919da2da4e42c9cc0c296ad8ee65f
bwill619 <bjw0619@gmail.com>
2022-01-31 16:59:15 -0500

    commit on main

--- src/App.js
+++ src/App.js
@@ -6,17 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h1 className='voice-header'>Voice Assistant</h1>
+        <p className='App-paragraph'>
+          This will be the voice assistant app.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
